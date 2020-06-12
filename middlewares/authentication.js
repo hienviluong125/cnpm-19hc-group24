d14128260e0c48d5665d45263a80b7547988477c
hienviluong125 <hienviluong125@gmail.com>
2020-06-12 22:54:02 +0700

    init an res.local.user variable from authentication middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,5 +1,6 @@
 module.exports = function (req, res, next) {
   if (req.user && typeof req.user !== 'undefined') {
+    res.locals.user = req.user;
     next();
   } else {
     req.flash('messages', [{ type: 'error', content: 'Please sign in first' }]);
